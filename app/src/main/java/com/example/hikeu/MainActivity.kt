a0ebe7eed77e7bb52a56975ec09f9b6596d2a0e1
Mauricio <A01194292@itesm.mx>
2021-12-02 12:36:44 -0600

    p

--- app/src/main/java/com/example/hikeu/MainActivity.kt
+++ app/src/main/java/com/example/hikeu/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.hikeu.databinding.ActivityMainBinding
@@ -18,6 +19,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val officialTrails = OfficialTrailsFragment()
+        val unOfficialTrails = UnOfficialTrails()
+        val homeFragment = HomeFragment()
+
+        makeCurrentFragment(HomeFragment)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.home -> makeCurrentFragment(homeFragment)
+                R.id.officialroutes -> makeCurrentFragment(officialTrails)
+                R.id.unofficialroutes -> makeCurrentFragment(unOfficialTrails)
+            }
+            true
+        }
+
         var viewModel = ViewModelProvider(this,HikeuViewModelFactory((application as
                 HikeuApp).database.MainDao())).get(HikeuViewModel::class.java)
 
@@ -28,10 +44,11 @@ class MainActivity : AppCompatActivity() {
         val users = viewModel.getAllUsers()
         Log.d("db", users.toString())
     }
-
-
-
-
-
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
